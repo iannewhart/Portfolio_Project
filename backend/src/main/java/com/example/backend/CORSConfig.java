3542cc06f6c9ea5af56a1a341d399cce67b26c62
Ian <iannewhart@gmail.com>
2026-03-04 13:29:25 -0500

    fix CORS to allow EC2 origin

--- backend/src/main/java/com/example/backend/CORSConfig.java
+++ backend/src/main/java/com/example/backend/CORSConfig.java
@@ -1,13 +1,16 @@
 package com.example.backend;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CORSConfig implements WebMvcConfigurer {
+    @Value("${ALLOWED_ORIGIN}")
+    private String allowedOrigin;
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:3000",allowedOrigin)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Specify allowed HTTP methods
                 .allowedHeaders("*") // Allow all headers
                 .allowCredentials(true) // Allow credentials (e.g., cookies, authorization headers)
