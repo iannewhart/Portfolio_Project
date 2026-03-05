024cf87b6063b1419bcfe617267a17ec5d6886dd
Ian <iannewhart@gmail.com>
2026-03-05 11:32:27 -0500

    Changed font and edited css

--- frontend/src/components/ContactForm.js
+++ frontend/src/components/ContactForm.js
@@ -1,32 +1,52 @@
 import {useEffect, useState} from "react";
 import '../css/Contact.css'
-
+import emailjs from '@emailjs/browser';
 function ContactForm() {
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [phonenumber, setPhoneNumber] = useState('');
     const [message, setMessage] = useState("");
+    const submitContact = async () => {
+        try{
+            await  emailjs.send('YOUR_SERVICE_ID',
+                'YOUR_TEMPLATE_ID',{
+                    from_name: name,
+                    from_email: email,
+                    phone_number: phonenumber,
+                    message: message,
+                },
+                'YOUR_PUBLIC_KEY'
+            );
+            alert('Message sent successfully!');
+        } catch (error) {
+            console.error('Failed to send:', error);
+            alert('Failed to send message. Please try again.');
+        }
+        };
+
     return (
         <div className="contact-container">
+            <h3 className="contact-title">Contact Us</h3>
+
             <form>
                 <div className="form-group">
-                    <label>Name:</label>
+                    <label className="form-label">Name:</label>
                     <input type="text" value={name} onChange={(e) => setName(e.target.value)} required/>
                 </div>
 
                 <div className="form-group">
-                    <label>Email:</label>
+                    <label className="form-label">Email:</label>
                     <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required/>
                 </div>
                 <div className="form-group">
-                    <label>Phone Number:</label>
+                    <label className="form-label">Phone Number:</label>
                     <input type="tel" value={phonenumber} onChange={(e) => setPhoneNumber(e.target.value)} required/>
                 </div>
                 <div className="form-group">
-                    <label>Message:</label>
+                    <label className="form-label" >Message:</label>
                     <textarea  type="message" value={message} onChange={(e) => setMessage(e.target.value)} required/>
                 </div>
-                <button type="button" className="contact-submit">Submit</button>
+                <button type="button" className="contact-submit" onClick={submitContact}>Submit</button>
             </form>
         </div>
 );
--- frontend/src/pages/Contact.js
+++ frontend/src/pages/Contact.js
@@ -6,14 +6,8 @@ import ContactForm from "../components/ContactForm";
 function Contact() {
     return (
         <div className="Contact">
-            <h1>
-                Contact Us
-            </h1>
             <h4>
                 Zoey HE (She/Her/Hers) - Founder
-            </h4>
-            <h4>
-                zoey@sunvio.video
             </h4>
             <ContactForm></ContactForm>
         </div>
